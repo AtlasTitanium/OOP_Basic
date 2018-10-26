ae48b419c6d41461ddabf02be85fb8aabfef08f7
Atlas <AtlasTheTitan77@gmail.com>
2018-10-26 14:40:44 +0200

    Les5Complete

--- TragischeBibliotheek/TragischeBibliotheek/Bibliotheek.cpp
+++ TragischeBibliotheek/TragischeBibliotheek/Bibliotheek.cpp
@@ -13,58 +13,68 @@ void Bibliotheek::toon() {
 		std::cout << "   Bibliotheek bevat: " << boek->naam << "		Status: " << boek->status << std::endl;
 	}
 	else {
-		std::cout << "   There are no books currently in the Library." << std::endl;
+		std::cout << "There are no books currently in the Library." << std::endl;
 	}
 	
 }
 
 void Bibliotheek::voegToe(std::string type) {
+	std::cout << std::endl << "Add Book : ";
 	if (boek != NULL) {
-		std::cout << "There's already a book in the library, please destroy the book before inserting a new one." << std::endl;
+		if (boek->status == "out") {
+			std::cout << "Book has been lend, please return the book and destroy it before inserting a new one" << std::endl;
+		}
+		else {
+			std::cout << "There's already a book in the library, please destroy the book before inserting a new one." << std::endl;
+		}
 	}
 	else {
 		boek = new Boek(type);
+		std::cout << "Book added!" << std::endl;
 	}
 }
 
 void Bibliotheek::leenUit() {
+	std::cout << std::endl << "Lend Out Book : ";
 	if (boek != NULL) {
 		if (boek->status == "out") {
-			std::cout << "   Book is already out of the Library." << std::endl;
+			std::cout << "Book is already out of the Library." << std::endl;
 		}
 		else {
 			boek->setStatus("out");
-			std::cout << "Book has been lend" << std::endl;
+			std::cout << "Book has been lend!" << std::endl;
 		}
 	}
 	else {
-		std::cout << "   There are no books currently in the Library." << std::endl;
+		std::cout << "There are no books currently in the Library." << std::endl;
 	}
 	
 }
 
 void Bibliotheek::brengTerug() {
+	std::cout << std::endl << "Return Book : ";
 	if (boek != NULL) {
 		if (boek->status == "in") {
 			std::cout << "   Book is already in the Library." << std::endl;
 		}
 		else {
 			boek->setStatus("in");
-			std::cout << "Book is back" << std::endl;
+			std::cout << "Book is back!" << std::endl;
 		}
 	}
 	else {
-		std::cout << "   There are no books currently in the Library." << std::endl;
+		std::cout << "There are no books currently in the Library." << std::endl;
 	}
 }
 
 void Bibliotheek::destroyBook() {
-	if (boek != NULL) {
+	std::cout << std::endl << "Destroy Book : ";
+	if (boek != NULL && boek->status == "in") {
 		boek = NULL;
-		std::cout << "Book has been destroyed" << std::endl;
+		std::cout << "Book has been destroyed!" << std::endl;
 	}
 	else {
-		std::cout << "   There are no books currently in the Library." << std::endl;
+		std::cout << "There are no books currently in the Library." << std::endl;
 	}
 }
 
